a618a10fe301abd543f3f067873435f09e9b394c
Nayana <nayanahn12@gmail.com>
2025-07-25 20:26:40 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; NayanaDB</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
